9f56b4d05eff84ce7735bb4b82668197be1c13e7
Mathew Juma <athias85@gmail.com>
2016-06-13 22:42:58 +0300

    Change to platform resources file

--- zf_application/app_models/zvs_platform_details/platformSchoolResources_model.php
+++ zf_application/app_models/zvs_platform_details/platformSchoolResources_model.php
@@ -129,12 +129,7 @@ class platformSchoolResources_Model extends Zf_Model {
                                                                                       </table>
                                                                                   </div>
                                                                              </div>
-                                                                        </div>';
-
-                                       }
-                                        
-            $zvs_resourcesGridView .='</div>          
-                                </div><div class="zvs-content-footer">
+                                                                        </div><div class="zvs-content-footer">
                                                                             <div class="row">';
                                                                     
                                                                    $zvs_resourcesGridView .= '<div class="col-lg-6 col-md-6 col-sm-9 col-xs-9">
@@ -143,6 +138,11 @@ class platformSchoolResources_Model extends Zf_Model {
                                                                                 
                                                      $zvs_resourcesGridView .=' </div>
                                                                         </div>';
+
+                                       }
+                                        
+            $zvs_resourcesGridView .='</div>          
+                                </div>';
              
              }
              
