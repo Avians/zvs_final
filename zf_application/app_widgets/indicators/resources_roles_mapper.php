ec440a08146d0b1b54b69c8825d1c24a552b14e7
MathewJuma <athias85@gmail.com>
2016-07-04 10:28:50 +0300

    change on resources_roles_mapper indicator

--- zf_application/app_widgets/indicators/resources_roles_mapper.php
+++ zf_application/app_widgets/indicators/resources_roles_mapper.php
@@ -3,12 +3,12 @@
         <button class="close" data-dismiss="alert"></button>
         You have not selected a school role. Check <strong>"Assign Resources to Roles"</strong> section and select a role, then re-assign resources!!
     </div>
-<?php} if(Zf_SessionHandler::zf_getSessionVariable('resources_roles_mapper') == 'role_mapping_error'){ ?>
+<?php }else if(Zf_SessionHandler::zf_getSessionVariable('resources_roles_mapper') == 'role_mapping_error'){ ?>
     <div class="alert alert-danger display-none error-fadeout">
         <button class="close" data-dismiss="alert"></button>
-        There are no active roles within any modules. You are not able to assign resources. Check later!!
+        There are no active resources within any modules or may be you did not select any resources from the ones provided. You action has been terminated, please check and try again!!
     </div>
-<?php } else if(Zf_SessionHandler::zf_getSessionVariable('resources_roles_mapper') == 'role_mapping_success'){ ?>
+<?php }else if(Zf_SessionHandler::zf_getSessionVariable('resources_roles_mapper') == 'role_mapping_success'){ ?>
     <div class="alert alert-success display-none success-fadeout">
         <button class="close" data-dismiss="alert"></button>
         You successfully assigned resources to a school role. You can now have an overview of assigned roles.
@@ -16,5 +16,3 @@
 <?php
 } 
 Zf_SessionHandler::zf_unsetSessionVariable("resources_roles_mapper");
-?>
-
